5120d578587e892788aaae1a5f7250568172b56d
pratik-walawalkar <65714445+pratik-walawalkar@users.noreply.github.com>
2020-06-11 20:42:27 +0200

    Added a python code to reformat bulk images

--- reformat_images.py
+++ reformat_images.py
@@ -1,19 +1,22 @@
-!/usr/bin/env python3
-
+ #!/usr/bin/env python3
 from PIL import Image
 import os
 import sys
-folder = "/home/student-04-e281aa9955e8/supplier-data/images/"
+source_folder = "/home/student-04-fc2745a2c9db/images/"
+destination_folder = "/opt/icons/"
+
 def reformat():
-    for item in os.listdir(folder):
-        f, e = os.path.splitext(item)
-        if e == ".tiff":
-            if os.path.isfile(folder+item):
-                #im.convert("RGB")
-                im = Image.open(folder+item)
-                im.convert("RGB")
-                f, e = os.path.splitext(item)
-                imResize = im.resize((600,400), Image.ANTIALIAS)
-                imResize.save(folder +f+ ".jpeg", quality=90)
+    for item in os.listdir(source_folder):
+        if os.path.isfile(source_folder+item):
+            im.convert("RGB")
+            im = Image.open(source_folder+item)
+            f, e = os.path.splitext(item)
+ 
+            #reformat image by resizing it to 128x128 and rotating it by 90 degrees
+            imResize = im.rotate(90).resize((128,128), Image.ANTIALIAS)
+
+            #Save the reformated image to another folder in JPEG format
+            imResize.save(destination_folder + f , 'JPEG', quality=90)
 
 reformat()
+
